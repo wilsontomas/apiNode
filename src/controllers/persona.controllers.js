4df33a8d99e8856badbe2973350a088fe13470b1
wilsontomas <wilsonpumm@gmail.com>
2021-10-03 23:39:57 -0300

    se agregaron las demas rutas y controladores

--- src/controllers/persona.controllers.js
+++ src/controllers/persona.controllers.js
@@ -4,10 +4,10 @@ const getPersonas =async (req,res)=>{
     try {
         const pool =await connection.getConnection();
         const result = await pool.request().execute('ObtenerPersonas');
-        console.log(result);
-        res.json(result);
+        console.log(result.recordset);
+        res.json(result.recordset);
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
     }
 }
 
@@ -18,11 +18,87 @@ const getPersonasCount =async (req,res)=>{
         console.log(result);
         res.json(result.recordset[0]);
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
     }
 }
 
+const getPersonaById = async (req,res)=>{
+    try {
+        const {id} =req.params;
+       const pool = await connection.getConnection();
+       const result =await pool.request()
+       .input("idPersona",connection.sql.Int,id)
+       .execute("ObtenerPersonaPorId");
+
+        console.log(result?.recordset[0]);
+        res.json(result?.recordset[0]);
+        //console.log(id)
+        //res.json(id)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const deletePersonaById = async (req,res)=>{
+    try {
+        const {id} =req.params;
+       const pool = await connection.getConnection();
+        await pool.request()
+       .input("IdPersona",connection.sql.Int,id)
+       .execute("EliminarPersona");
+
+        console.log("Se elimino un registro");
+        res.json("Se elimino un registro");
+        //console.log(id)
+        //res.json(id)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const addPersona = async (req,res)=>{
+    try {
+        const {Nombre,Apellido,Idprovincia} =req.body;
+       const pool = await connection.getConnection();
+        await pool.request()
+       .input("Nombre",connection.sql.VarChar,Nombre)
+       .input("Apellido", connection.sql.VarChar,Apellido)
+       .input("Idprovincia",connection.sql.Int,Idprovincia)
+       .execute("InsertarPersona");
+
+        console.log("Se agrego un registro");
+        res.json("Se agrego un registro");
+        //console.log(id)
+        //res.json(id)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const editPersona = async (req,res)=>{
+    try {
+        const {IdPersona, Nombre,Apellido,provincia} =req.body;
+       const pool = await connection.getConnection();
+        await pool.request()
+       .input("IdPersona",connection.sql.Int,IdPersona)
+       .input("Nombre",connection.sql.VarChar,Nombre)
+       .input("Apellido", connection.sql.VarChar,Apellido)
+       .input("provincia",connection.sql.Int,provincia)
+       .execute("EditarPersona");
+
+        console.log("Se edito un registro");
+        res.json({IdPersona, Nombre,Apellido,provincia});
+        //console.log(id)
+        //res.json(id)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 module.exports.controllers ={
     getPersonas,
-    getPersonasCount
+    getPersonasCount,
+    getPersonaById,
+    deletePersonaById,
+    editPersona,
+    addPersona
 }
--- src/routes/persona.routes.js
+++ src/routes/persona.routes.js
@@ -5,8 +5,10 @@ const router = Router();
 //routes
 router.get('/persona', controllers.getPersonas);
 router.get('/persona/count',controllers.getPersonasCount)
-
-
+router.get('/persona/get/:id',controllers.getPersonaById)
+router.post('/persona/add',controllers.addPersona)
+router.put('/persona/edit',controllers.editPersona)
+router.delete('/persona/eliminar/:id',controllers.deletePersonaById)
 
 
 module.exports.personaRoutes =router;
